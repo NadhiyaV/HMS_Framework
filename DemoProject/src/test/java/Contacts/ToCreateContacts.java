cc9d99d35999ccd071536a8ac47c3f370f0c62e3
DELL <DELL@DESKTOP-07J22P1>
2024-03-22 10:33:00 +0530

    demo commit

--- DemoProject/src/test/java/Contacts/ToCreateContacts.java
+++ DemoProject/src/test/java/Contacts/ToCreateContacts.java
@@ -4,7 +4,7 @@ public class ToCreateContacts {
 
 	public static void main(String[] args) {
 
-		System.out.println("Contacts createdsuccessfully");
+		System.out.println("New Contacts createdsuccessfully");
 	}
 
 }

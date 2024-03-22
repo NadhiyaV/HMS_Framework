4a75272e2111527500b446572363953e6caa25a0
DELL <DELL@DESKTOP-07J22P1>
2024-03-22 11:00:22 +0530

    fourth commit

---
+++ DemoProject/src/test/java/Organizations/ToCreateOrganizationwithContacts.java
@@ -0,0 +1,10 @@
+package Organizations;
+
+public class ToCreateOrganizationwithContacts {
+
+	public static void main(String[] args) {
+		System.out.println("New Organoization created");
+
+	}
+
+}

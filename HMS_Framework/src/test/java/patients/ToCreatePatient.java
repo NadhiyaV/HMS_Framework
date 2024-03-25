f37ef90f87a4eda1bfffee4c4ff9ee74d1ffa35a
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-03-25 18:25:32 +0530

    pull push created

--- HMS_Framework/src/test/java/patients/ToCreatePatient.java
+++ HMS_Framework/src/test/java/patients/ToCreatePatient.java
@@ -4,7 +4,7 @@ public class ToCreatePatient {
 
 	public static void main(String[] args) {
 
-             System.out.println("patient created successfully");
+             System.out.println("patient  was created successfully");
 	}
 
 }
